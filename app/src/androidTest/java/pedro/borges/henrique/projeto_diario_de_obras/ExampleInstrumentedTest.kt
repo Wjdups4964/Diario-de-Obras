de82c99b887127cbc5e7810b430ec32aeb8a1f0f
Pedro Henrique Borges Silva <engpedro.run@gmail.com>
2026-09-09 22:17:58 -0300

    Estrutura inicial do aplicativo Diario de Obras

---
+++ app/src/androidTest/java/pedro/borges/henrique/projeto_diario_de_obras/ExampleInstrumentedTest.kt
@@ -0,0 +1,24 @@
+package pedro.borges.henrique.projeto_diario_de_obras
+
+import androidx.test.platform.app.InstrumentationRegistry
+import androidx.test.ext.junit.runners.AndroidJUnit4
+
+import org.junit.Test
+import org.junit.runner.RunWith
+
+import org.junit.Assert.*
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+@RunWith(AndroidJUnit4::class)
+class ExampleInstrumentedTest {
+    @Test
+    fun useAppContext() {
+        // Context of the app under test.
+        val appContext = InstrumentationRegistry.getInstrumentation().targetContext
+        assertEquals("pedro.borges.henrique.projeto_diario_de_obras", appContext.packageName)
+    }
+}
